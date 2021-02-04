380530a71a8c79f2c8f1aacf55cee5a3585efa39
Michael Brown <mikeb@360replays.com>
2021-02-03 16:25:45 -0800

    add test support for .net 5

--- AnyConfig/AnyConfig.Tests/RuntimeEnvironmentTests.cs
+++ AnyConfig/AnyConfig.Tests/RuntimeEnvironmentTests.cs
@@ -12,6 +12,8 @@ namespace AnyConfig.Tests
             var detectedRuntime = RuntimeEnvironment.DetectRuntime();
 #if NETFRAMEWORK
             Assert.AreEqual(RuntimeFramework.DotNetFramework, detectedRuntime.DetectedRuntimeFramework);
+#elif NET5_0
+            Assert.AreEqual(RuntimeFramework.DotNet5, detectedRuntime.DetectedRuntimeFramework);
 #elif NETCOREAPP
             Assert.AreEqual(RuntimeFramework.DotNetCore, detectedRuntime.DetectedRuntimeFramework);
 #else
